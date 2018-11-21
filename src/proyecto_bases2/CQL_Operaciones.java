bba11f5bcbefc031fe1f0642f6fd5881efa5e308
Karen Medina <Karen Medina@DESKTOP-JRBGNE9>
2018-11-21 16:43:25 -0600

    3...

--- src/proyecto_bases2/CQL_Operaciones.java
+++ src/proyecto_bases2/CQL_Operaciones.java
@@ -5,43 +5,56 @@
  */
 package proyecto_bases2;
 
-/**
- *
- * @author Karen Medina
- */
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.exceptions.NoHostAvailableException;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.naming.ConfigurationException;
+import jdk.nashorn.internal.runtime.JSType;
+
+
 public class CQL_Operaciones {
     
     private static Cluster cluster;
     private static Session session;
     
-    private static void IniciarConexion(){
+    public static Cluster connect(String node){
+        return Cluster.builder().addContactPoint(node).build();
+    }
+
+    private static void IniciarConexion() {
         try {
-            cluster = connect("192.168.0.1");
-        } catch (NoMostAvailableException e) {
-            throw new  RuntimeException(e);
-            
+            cluster = connect("127.0.0.1");
+        } catch (NoHostAvailableException e) {
+            throw new RuntimeException(e);
+
         }
     }
-    
-    public static void IniciarSesion(String KeySpaceName){
+
+    public static void IniciarSesion(String KeySpaceName) {
         try {
             session = cluster.connect(KeySpaceName);
-        } catch (NoMostAvailableException e) {
+        } catch (NoHostAvailableException e) {
             throw new RuntimeException(e);
         }
     }
-    
-    public static void Crear_keySpace(String KeySpaceName){
+
+    public static void Crear_keySpace(String KeySpaceName) {
         session = cluster.connect();
-        session.execute("CREATE KEYSPACE "+ KeySpaceName + "WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1}")
+        session.execute("CREATE KEYSPACE " + KeySpaceName + "WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1}");
     }
-    
-    public static void Eliminar_keySpace (String KeySpaceName){
-        session.execute("DROP KEYSPACE "+ KeySpaceName);
+
+    public static void Eliminar_keySpace(String KeySpaceName) {
+        session.execute("DROP KEYSPACE " + KeySpaceName);
     }
-    
-    public static void finalizarConexion(){
+
+    public static void finalizarConexion() {
         session.close();
         cluster.close();
     }
+    
 }
